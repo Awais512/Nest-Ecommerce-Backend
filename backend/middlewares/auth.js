9c1e5de196d4681425afec20325a41e2342ab027
awais512 <raza.awais25@gmail.com>
2022-06-06 09:58:49 +0500

    Protect User Routes and Admin Routes using auth middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,35 @@
+import ErrorResponse from '../utils/ErrorResponse.js';
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import User from '../models/UserModel.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorResponse('Please Login to access this resource', 401));
+  }
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
+
+const admin = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `Role: ${req.user.role} is not allowed to access this resouce `,
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
+
+export { protect, admin };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,9 +6,14 @@ import {
   updateProduct,
   deleteProduct,
 } from '../controllers/productController.js';
+import { admin, protect } from '../middlewares/auth.js';
 const router = express.Router();
 
-router.route('/').post(createProduct).get(getProducts);
-router.route('/:id').get(getProduct).put(updateProduct).delete(deleteProduct);
+router.route('/').post(protect, admin('admin'), createProduct).get(getProducts);
+router
+  .route('/:id')
+  .get(getProduct)
+  .put(protect, admin('admin'), updateProduct)
+  .delete(protect, admin('admin'), deleteProduct);
 
 export default router;
